8260a14aa2f5de47f9d31873dedccb203902d0a3
Vikrift <alexievviktor1@gmail.com>
2024-05-04 18:31:13 +0300

    connected to mysql database

--- AutoTech/AutoTech/settings.py
+++ AutoTech/AutoTech/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'AutoTech.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'autotech',
+        'USER': 'viktor',
+        'PASSWORD': 'Eren',
+        'HOST':'176.12.32.42',
+        'PORT':'2503',
     }
 }
 
