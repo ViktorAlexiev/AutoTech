35464d98880e75e595f9b6b3e953a19701d28ac8
Vikrift <alexievviktor1@gmail.com>
2024-05-04 14:50:51 +0300

    added simple navbar

--- AutoTech/frontend/src/App.js
+++ AutoTech/frontend/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div class="navbar">
+        <button class="btn" href="newForm">Нова работна карта</button>
+        <button class="btn" href="searchForm">Търсене на работна карта</button>
+        <a class="link" href="login">Изход</a>
+      </div> 
     </div>
   );
 }
