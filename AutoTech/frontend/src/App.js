3eb61fa73b43c458c3e13e829f89648d6c3d2e45
Vikrift <alexievviktor1@gmail.com>
2024-06-05 23:39:44 +0300

    the navbar only refreshes the page component now; exported the Access database to MySQL and made new queries

--- AutoTech/frontend/src/App.js
+++ AutoTech/frontend/src/App.js
@@ -3,26 +3,20 @@ import Navbar from "./Navbar"
 import Statistics from "./subpages/Statistics";
 import NewCard from "./subpages/NewCard";
 import Search from "./subpages/Search";
-
 import './style.css';
+import { Route, Routes} from "react-router-dom"
 
 function App() {
-  let Component
-  switch(window.location.pathname){
-    case "/statistics":
-      Component = Statistics;
-      break;
-    case "/newCard":
-      Component = NewCard;
-      break;
-    case "/search":
-      Component = Search;
-      break;
-  }
   return  (
     <>
       <Navbar />
-      <Component />
+      <div>
+        <Routes>
+          <Route path="/statistics" element={<Statistics />}/>
+          <Route path="/newCard" element={<NewCard />}/>
+          <Route path="/search" element={<Search />}/>
+        </Routes>
+      </div>
     </>
   )
   
--- AutoTech/frontend/src/subpages/Statistics.js
+++ AutoTech/frontend/src/subpages/Statistics.js
@@ -1,5 +1,10 @@
 export default function Statistics(){
 
-    return( <h2>Statistics</h2>
+    return( 
+    <div>
+        <h2>Извършени ремонти:</h2>
+        <br></br>
+        <h2>Клиенти:</h2>
+    </div>
     )
 }
